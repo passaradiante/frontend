8c982beee86e398a6d2d4f550752767006a9d909
Emilio-Falcao <emilio.falcao@gmail.com>
2019-10-28 10:10:42 -0300

    config

--- src/app/mock-produtos.ts
+++ src/app/mock-produtos.ts
@@ -6,44 +6,30 @@ export const PRODUTOS: ProdutoCadastro[] = [
         id: 1,
         nome: 'Cadeira de roda',
         descricao: 'Para pessoas que tem problemas em locomoção',
-<<<<<<< HEAD
-        valor: '50,00',
-        fotoUrl: './assets/images/cadeiraroda.png'
-=======
-        valor: '50',
+        valor: "50,00",
         fotoUrl: './assets/images/cadeiraroda.png',
         telefone:'(85)956545412',
         email:'tiago.dantas95@gmail.com'
->>>>>>> 6403f018371bb1ee81ee92460e36a351ec18ad22
     },
     {
         id: 2,
         nome: 'Muleta',
         descricao: 'Para pessoas que tem problemas nas pernas',
-<<<<<<< HEAD
-        valor: '90,00',
-        fotoUrl: './assets/images/muleta.png'
-=======
-        valor: '90',
+        valor: "90,00",
         fotoUrl: './assets/images/muleta.png',
         telefone:'(85)9565434312',
         email:'saulo@gmail.com'
->>>>>>> 6403f018371bb1ee81ee92460e36a351ec18ad22
 
     },
     {
         id: 3,
         nome: 'Respirador',
         descricao: 'Para pessoas que tem problemas para respirar',
-<<<<<<< HEAD
-        valor: '150,00',
-        fotoUrl: './assets/images/respirador.png'
-=======
-        valor: '150',
+        valor: "150,00",
         fotoUrl: './assets/images/respirador.png',
+      
         telefone:'(85)954587122412',
         email:'emilio@gmail.com'
->>>>>>> 6403f018371bb1ee81ee92460e36a351ec18ad22
 
     }
 ];
