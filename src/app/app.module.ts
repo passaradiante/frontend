822ae4623ea3c071bc7cdd9e673a1532e81ef440
sholiveira <sholiveira.git@gmail.com>
2019-11-03 22:25:41 -0300

    Conflito corrigido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { LoginComponent } from './pages/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BarraMenuComponent } from './components/barra-menu/barra-menu.component';
-import { TelaInicialComponent } from './pages/telaInicial/tela-inicial.component';
-import { ProdutoComponent } from './pages/anuncio/produto/produto.component';
-import { ServicoComponent } from './pages/anuncio/servico/servico.component';
-import { AnuncioComponent } from './pages/anuncio/anuncio.component';
-import { CadastroProdutoComponent } from './pages/cadastro-produto/cadastro-produto.component';
-import { BarraOcultaComponent } from './pages/barra-oculta/barra-oculta.component';
-=======
 
 import { BarraMenuComponent } from './core/components/barra-menu/barra-menu.component';
 
@@ -35,7 +23,6 @@ import { MinhaContaComponent } from './pages/usuario/minha-conta/minha-conta.com
 import { ProdutoService } from './core/services/produto.service';
 import { CategoriaService } from './core/services/categoria.service';
 
->>>>>>> 1ea7415477e74e704ad55b78d4b0e74193a9eb40
 
 @NgModule({
   declarations: [
@@ -45,21 +32,11 @@ import { CategoriaService } from './core/services/categoria.service';
     HomeComponent,
     CadastroComponent,
     LoginComponent,
-<<<<<<< HEAD
-    BarraMenuComponent,
-    TelaInicialComponent,
-    ProdutoComponent,
-    ServicoComponent,
-    AnuncioComponent,
-    CadastroProdutoComponent,
-    BarraOcultaComponent,
-=======
     DetalheComponent,
     ProdCadComponent,
     ProdListComponent,
     MinhaContaComponent
   
->>>>>>> 1ea7415477e74e704ad55b78d4b0e74193a9eb40
   ],
   imports: [
     FormsModule,
